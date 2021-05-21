6df6e94ee98790574fbaaa6e7f57b31f57e1293f
idosh <idoshziv@gmail.com>
2021-05-21 18:08:03 +0300

    fixed errors and added edge cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int [] a = {1,2,3,4};
-        int [] b = {4,2,3,1};
-        int [] b1 = {0,2,3,1};
-        int [] b2 = {5,2,3,1};
-        System.out.println("is permutation a & b: "  + isPermutation(a,b) + " needs to be true");
-        System.out.println("is permutation b1 & b2: "  + isPermutation(b1,b2) + " needs to be false");
+        int [][] ido = {
+                {0,1,0,0,0,1},
+                {1,0,0,1,1,1},
+                {0,0,0,0,0,1},
+                {1,1,1,1,1,1},
+                {0,1,0,1,0,1},
+                {0,1,0,0,0,0}
+        };
+        System.out.println(isSink(ido) +  " -1");
+//        int [] a = {1,2,3,4};
+//        int [] b = {4,2,3,1};
+//        int [] b1 = {0,2,3,1};
+//        int [] b2 = {5,2,3,1};
+//        int [] q101 = new int[]{5,5,5,-5},  q102 =new int[]{-5,5,-5,5};
+//        int [] q11 = new int[]{-5,5,5,-5} ,q12 = new int[]{5,5,-5,5};     // 11
+//        int [] q141 = new int[]{1,2,4,4}, q142 = new int[]{1,2,4,2};        // 14
+//        System.out.println("is permutation a & b: "  + isPermutation(a,b) + " needs to be true");
+//        System.out.println("is permutation b1 & b2: "  + isPermutation(b1,b2) + " needs to be false");
+//        System.out.println("is permutation q101 & q102: "  + isPermutation(q101,q102) + " needs to be false");
+//        System.out.println("is permutation q11 & q12: "  + isPermutation(q11,q12) + " needs to be false");
+//        System.out.println("is permutation q141 & q142: "  + isPermutation(q141,q142) + " needs to be false");
+//        testIsSink();/
+//        testSize();
     }
 
     /**
@@ -47,12 +64,25 @@ public class Main {
      * @param mat -> the matrix
      * @return sink row number or -1 if there is no sink
      */
+ /*
     public static int isSink (int [][] mat){
+        boolean [] sinkDisqualified = new boolean[mat.length]; // setting a blacklist of cells
+        for (int r = 0; r< sinkDisqualified.length; r++)
+            sinkDisqualified[r] = false; // set the array
         int i =0,j = 0;
         while (i < mat.length){
+            if (sinkDisqualified[i]) // the row is already false
+            {
+                i++;
+                continue;
+            }
             if (j < mat.length){ // as long as we are inside the row
                 if (mat[i][j] == 0){ // if the cell is 0 we can continue
+                    if (i != j) //
+                        sinkDisqualified[j] = true;
                     if (mat[j][i] == 1 || i == j){ // whether the col is 1 or the cell int [k][k] is 0
+                        if (i != j) //
+                            sinkDisqualified[j] = true; // the j row is disqualified because [j][i] has a 1
                         j++; // if so continue with the col
                     }
                     else { // if not the row is not good, return to first col and proceed to next row
@@ -69,6 +99,25 @@ public class Main {
                 return i;
         }
         // edge case -> the mat size is [0][0]
+        return -1; // we got here because we couldn't find any match. return -1
+    }
+*/
+
+    public static int isSink (int [][] mat) {
+        int row = 0, col = 0, sumR = 0, sumC = 0;
+//        I'll go over the mat once. if a cell is not complaint I'm moving to the next row -> O(n)
+        while (row < mat.length && col < mat[0].length) {
+            if (mat[row][col] == 0)  // if the cell is good we can continue with this row
+                col++;
+            else  // proceed to the next
+                row++;
+        }
+        if (row == mat.length) return -1; // row out of bound means no sink
+        for (int i = 0; i < mat.length; i++) { // sum the row and col of the findings, O(n) -> iterate once
+            sumC += mat[i][row]; // sum up the column, the number of row is the one of the col so we can use it like that
+            sumR += mat[row][i]; // sum up the row
+        }
+        if (sumC == mat.length - 1 && sumR == 0) return row;
         return -1; // we got here because we couldn't find any match. return -1
     }
 
@@ -142,7 +191,7 @@ public class Main {
         System.out.println("g -> " +maximalDrop(g) + " ");
     }
     // testing ex2
-    private static void testIsSync(){
+    private static void testIsSink(){
         //        printArr();
         int [] [] a =
                 {
@@ -178,20 +227,22 @@ public class Main {
                         {0,0,0,0,0,0}
                 };
         System.out.println("b ->" +isSink(b));
-        int [] [] c = {{0,1,0,1,1,0},
+        int [] [] c = {
+                {0,1,0,1,1,0},
                 {1,0,1,1,0,0},
                 {0,0,0,1,0,1},
                 {0,0,0,0,0,0},
                 {1,0,1,1,0,0},
                 {0,1,0,1,1,1}};
-        int [] [] d = {{0,1,0,0,0,1},
+        int [] [] d = {
+                {0,1,0,0,0,1},
                 {1,0,0,1,1,1},
                 {0,0,0,0,0,0},
                 {1,1,1,1,1,1},
                 {0,1,0,1,0,1},
                 {1,0,0,0,1,0}};
-        System.out.println("c ->" +isSink(c));
-        System.out.println("d ->" +isSink(d));
+        System.out.println("A ->" +isSink(c));
+        System.out.println("B ->" +isSink(d));
 //        createArr();
 //        System.out.println(a);
 
@@ -206,6 +257,8 @@ public class Main {
      * @return the size of a "stain" in the matrix.
      */
     public static int size (boolean[][] mat, int x, int y) {
+        if (mat.length == 0)
+            return 0;
         boolean [][] visited = new boolean[mat.length][mat[0].length];
         fillBlanks(visited,0,0);
         System.out.println("visited:");
@@ -252,7 +305,6 @@ public class Main {
             fillBlanks(mat,x+1,y);
             fillBlanks(mat,x,y+1);
         }
-
     }
     public static void testSize(){
         createBooleanTrueArr(5,5);
@@ -289,6 +341,11 @@ public class Main {
         System.out.println("mat3,1,3 -> " + size(mat3,1,3) + " needs to be 5");
         System.out.println("mat3,4,0 -> " + size(mat3,4,0) + " needs to be 4");
         System.out.println("mat3,4,4 -> " + size(mat3,4,4) + " needs to be 0");
+        int l = size(new boolean[][]{
+                {true,false},
+                {false,true}
+        }, 0, 0);
+        System.out.println(2 + " " + l);
     }
 
     /**
@@ -300,7 +357,9 @@ public class Main {
     public static boolean isPermutation (int [] a, int [] b){
         if (a.length != b.length) //edge case -> the arrays are not the same lenght
             return false;
-        return isPermutation(a,0, b);
+        boolean [] isVisited = new boolean[b.length];
+        fillIsVisited(isVisited,0);
+        return isPermutation(a,0, b,isVisited);
     }
 
     /**
@@ -310,15 +369,15 @@ public class Main {
      * @param b -> the second array
      * @return true if all the cells in a are in b
      */
-    private static boolean isPermutation (int [] a,int ai, int [] b){
+    private static boolean isPermutation (int [] a,int ai, int [] b,boolean [] isVisited){
         if (ai == a.length -1) //check last cell
         {
             if (a.length > 1)
-                return isPermutation(a,ai,b,0);
+                return isPermutation(a,ai,b,0, isVisited);
             return a[ai] == b[ai]; // edge case -> both array are one cell length
         }
         if (ai < a.length){
-            return isPermutation(a,ai,b,0) && isPermutation(a,ai+1,b);
+            return isPermutation(a,ai,b,0, isVisited) && isPermutation(a,ai+1,b, isVisited);
         }
         return false; // edge case -> index oob -> null array
     }
@@ -331,10 +390,27 @@ public class Main {
      * @param bi -> array two index
      * @return true if found cell in b
      */
-    private static boolean isPermutation (int [] a,int ai, int [] b, int bi){
-        if (bi < b.length)
-            return a[ai] == b[bi] || isPermutation(a,ai,b,bi+1);
+    private static boolean isPermutation (int [] a,int ai, int [] b, int bi, boolean [] isVisited){
+        if (bi < b.length){
+            if (a[ai] == b[bi] && !isVisited[bi]){ // validate that we don't repeat steps
+                isVisited[bi] = true;
+                return true;
+            }
+            return isPermutation(a,ai,b,bi+1, isVisited);
+        }
         return false;
+    }
+
+    /**
+     * private method that fills the array with the values of false
+     * @param isVisited -> the given array
+     * @param i -> the index
+     */
+    private static void fillIsVisited(boolean [] isVisited, int i){
+        if (i < isVisited.length){
+            isVisited[i] = false;
+            fillIsVisited(isVisited,i+1);
+        }
     }
 
 
